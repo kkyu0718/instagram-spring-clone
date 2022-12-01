fd8b8b0f706bd20a46ac7a356e28cf83f712a6b8
kkyu0718 <kyuwon0718@gmail.com>
2022-12-02 00:01:28 +0900

    [Feat] 프로젝트 세팅

---
+++ instagram-clone/src/main/java/instagramclone/InstagramCloneApplication.java
@@ -0,0 +1,14 @@
+package instagramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+		SpringApplication.run(InstagramCloneApplication.class, args);
+	}
+
+}
---
+++ instagram-clone/src/test/java/instagramclone/InstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
