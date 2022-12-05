35602a3321ce4cd422e5c8e9e009f2c6165ad417
kkyu0718 <kyuwon0718@gmail.com>
2022-12-05 17:17:55 +0900

    #2-jpaAuditing 추가
    임시로 "tempory user"로 설정해뒀습니다.

--- instagram-clone/src/main/java/instagramclone/domain/User.java
+++ instagram-clone/src/main/java/instagramclone/domain/User.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -19,6 +20,7 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class User {
     @Id
