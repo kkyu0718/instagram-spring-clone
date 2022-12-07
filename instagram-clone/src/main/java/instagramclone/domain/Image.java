1eeb38253f368bd5e763eea2494c0c55a61afab8
kkyu0718 <kyuwon0718@gmail.com>
2022-12-07 19:23:12 +0900

    #5-image-feed 매핑 및 생성자 작성

--- instagram-clone/src/main/java/instagramclone/domain/Image.java
+++ instagram-clone/src/main/java/instagramclone/domain/Image.java
@@ -1,4 +1,48 @@
 package instagramclone.domain;
 
+import jakarta.persistence.*;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Table(indexes = {
+        @Index(columnList = "feed")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false) private String imageUrl;
+
+    @ManyToOne()
+    private Feed feed;
+
+    protected Image() {}
+
+    private Image(String imageUrl, Feed feed) {
+        this.imageUrl = imageUrl;
+        this.feed = feed;
+    }
+
+    public static Image of(String imageUrl, Feed feed) {
+        return new Image(imageUrl, feed);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return id.equals(image.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
