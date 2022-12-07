29e947e8540e4471689135b6d41edb120b9941a3
kkyu0718 <kyuwon0718@gmail.com>
2022-12-07 19:25:22 +0900

    #4-feed-image 매핑 및 생성자 작성

--- instagram-clone/src/main/java/instagramclone/domain/Feed.java
+++ instagram-clone/src/main/java/instagramclone/domain/Feed.java
@@ -6,6 +6,10 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 @Getter
 @ToString
 @Table(indexes = {
@@ -21,4 +25,30 @@ public class Feed {
     private Long id;
 
     @Setter @Column(nullable = false) private String content;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "feed")
+    private Set<Image> images = new HashSet<>();
+
+    protected Feed() {}
+
+    private Feed(String content) {
+        this.content = content;
+    }
+
+    public static Feed of(String content) {
+        return new Feed(content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feed feed = (Feed) o;
+        return id.equals(feed.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
