5bee217cd2d6b4f3e7987476108a86b82f24a103
kkyu0718 <kyuwon0718@gmail.com>
2022-12-10 00:15:07 +0900

    #13-Like 도메인 작성

---
+++ instagram-clone/src/main/java/instagramclone/domain/Like.java
@@ -0,0 +1,26 @@
+package instagramclone.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table
+@EntityListeners(AuditingField.class)
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "feedId")
+    private Feed feed;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+}
