6e20e578d8b2789327bbc26efb57d37aa5715d57
kkyu0718 <kyuwon0718@gmail.com>
2022-12-12 20:45:57 +0900

    #20-userRepository 추가

---
+++ instagram-clone/src/main/java/instagramclone/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package instagramclone.repository;
+
+import instagramclone.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserAccount, Long> {
+
+}
