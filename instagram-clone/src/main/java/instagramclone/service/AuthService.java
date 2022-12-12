fa4104d7eacb9c2f7b1e173949ef082d8f79b601
kkyu0718 <kyuwon0718@gmail.com>
2022-12-12 20:45:38 +0900

    #20-user service 작성

---
+++ instagram-clone/src/main/java/instagramclone/service/AuthService.java
@@ -0,0 +1,27 @@
+package instagramclone.service;
+
+import instagramclone.domain.UserAccount;
+import instagramclone.dto.UserAccountDto;
+import instagramclone.dto.request.SignInRequest;
+import instagramclone.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class AuthService {
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserAccountDto saveUser(String name, String email, String password) {
+        UserAccount userAccount = UserAccount.of(name, email, password);
+        UserAccount save = userRepository.save(userAccount);
+        return UserAccountDto.from(save);
+    }
+}
