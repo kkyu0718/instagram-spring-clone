cea1ba9615940a662118c254466590b100c66326
kkyu0718 <kyuwon0718@gmail.com>
2022-12-21 18:24:19 +0900

    #28-feed service에서 image 는 구현 제외
    image 는 s3 bucket 설정이 필요하기 때문에 이후에 구현하기로 합니다.

--- instagram-clone/src/main/java/instagramclone/service/FeedService.java
+++ instagram-clone/src/main/java/instagramclone/service/FeedService.java
@@ -3,13 +3,17 @@ package instagramclone.service;
 import instagramclone.domain.Feed;
 import instagramclone.domain.Image;
 import instagramclone.dto.FeedDto;
+import instagramclone.exception.CustomException;
+import instagramclone.exception.ErrorCode;
 import instagramclone.repository.FeedRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,9 +24,21 @@ public class FeedService {
     private final FeedRepository feedRepository;
 
     @Transactional
-    public FeedDto createFeed(String content, Set<Image> images) {
-        Feed feed = Feed.of(content, images);
+    public FeedDto createFeed(String content, Set<String> images) {
+//        Set<Image> imageSet = images
+//                .stream()
+//                .map(Image::of)
+//                .collect(Collectors.toUnmodifiableSet());
+        //        Feed feed = Feed.of(content, imageSet);
+        Feed feed = Feed.of(content);
         Feed savedFeed = feedRepository.save(feed);
+
         return FeedDto.from(savedFeed);
+    }
+
+    @Transactional
+    public Optional<FeedDto> getFeed(Long id) {
+        Optional<FeedDto> feed = feedRepository.findById(id).map(FeedDto::from);
+        return feed;
     }
 }
